89cead93eaefc500c072c53a2f5167b863d8f473
Jack Laird <jack.laird0@gmail.com>
2018-07-10 14:36:49 -0600

    Set up tables for server

--- db/migrations/20180710142105_initial.js
+++ db/migrations/20180710142105_initial.js
@@ -1,8 +1,32 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('zones', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('lowTemp');
+      table.string('highTemp');
+      
+      table.timestamps(true, true);
+    }),
+    
+    knex.schema.createTable('plants', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('scientificName');
+      table.string('care');
+      table.string('moisture');
+      table.string('light');
+      table.string('maintenance');
+      table.foreign('zone_id')
+        .references('zones.id');
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('plants'),
+    knex.schema.dropTable('zones')
+  ]);
 };
