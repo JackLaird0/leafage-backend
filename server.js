e609371e8e97dd5467114ab38533aaac84368acf
Jack Laird <jack.laird0@gmail.com>
2018-07-10 15:14:42 -0600

    Make get endpoint for zones

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('api/v1/zones/', (request, response) => {
+  database('zones').select()
+    .then( zones => {
+      response.status(200).json(zones)
+    })
+    .catch( error => {
+      response.status(500).json({ error })
+    })
+});
+
 app.listen(router.get('port'), () => {
   console.log(`Leafage's Backend is running on ${router.get('port')}.`)
 });
