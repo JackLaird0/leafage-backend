3ed07439d9b30e2b52cb39ed36a66a7f5fd127f4
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-10 21:55:57 -0600

    Add scraper and resulting data from scraping 176 pages of plants for all urls

--- scraper.js
+++ scraper.js
@@ -1,29 +1,46 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
+const fs = require('fs')
 
-let allPlants = []
-for (let i = 1; i < 117; i++) {
-  nightmare
-    .goto(`https://www.finegardening.com/plant-guide?paged=${i}`)
-    .wait('.content-browser__results')
-    .evaluate(() => {
-      const cells = document.querySelectorAll('.content-browser__linked-image')
-      var list = [].slice.call(cells); 
-      const pageResults = list.map(function (node, index) {
-        return `${node}`
-      });
-      const newPlants = pageResults.map(result => {
-        nightmare
-          .goto(result)
-          .wait('')
-      })
-      return allPlants
-    })
-    .end()
-    .then(function (result) {
-      allPlants.push(...)
-    })
-    .catch(function (error) {
-      console.error('Search failed:', error);
-    });
+let iterable = []
+for (let i = 1; i < 177; i++) {
+  iterable.push(i)
 }
+let allURLs = []
+
+iterable.reduce(function (accumulator, pageNumber) {
+  return accumulator.then(function (results) {
+    return nightmare.goto(`https://www.finegardening.com/plant-guide?paged=${pageNumber}`)
+      .wait('.content-browser__results')
+      .evaluate(() => {
+        const cells = document.querySelectorAll('.content-browser__linked-image')
+        var list = [].slice.call(cells);
+        const pageResults = list.map(function (node, index) {
+          return `${node}`
+        });
+        return pageResults
+      })
+      .then(function (result) {
+        allURLs.push(...result)
+      })
+  });
+}, Promise.resolve([])).then(function (results) {
+  console.dir(allURLs);
+  let setupFilePath = __dirname + '/./plantdata.js'
+  fs.writeFile(setupFilePath, JSON.stringify(allURLs))
+});
+
+
+
+// const newPlants = pageResults.map(result => {
+      //   nightmare
+      //     .goto(result)
+      //     .wait('.article__wrap')
+      //     .evaluate(() => {
+      //       // const name = document.querySelector('.article__title').innerText
+      //       // const scientificName = document.querySelector('em')
+      //       // const care = doc
+      //       return document.querySelectorAll('p')
+      //     })
+      // })
+      // return newPlants
