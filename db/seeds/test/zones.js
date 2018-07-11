6043a4590374b24dec88d215da5c6ff5e62cbc56
Jack Laird <jack.laird0@gmail.com>
2018-07-10 19:51:49 -0600

    Update seeding test file to match the dev

--- db/seeds/test/zones.js
+++ db/seeds/test/zones.js
@@ -1,25 +1,20 @@
 const { zoneData, plantData } = require('../seed-data'); 
 
 exports.seed = function (knex, Promise) {
-  const zones = zoneData.map(zone => {
-    return knex('zones').insert(zone, 'id')
-  });
   return knex('plants').del()
     .then(() => knex('zones').del())
     .then(() => {
-      return Promise.all([
-        
-          ...zones
+        return Promise.all(zoneData.map(zone => {
+            return knex('zones').insert(zone, 'id')
+          }))
           .then(zones => {
-            console.log(zones)
             plantData.forEach(plant => {
-              plant.zone_id = zones[0]
+              plant.zone_id = zones[0][0]
             })
             return knex('plants').insert(plantData)
           })
           .then(() => console.log('Seeding complete!'))
           .catch(error => console.log(`Error seeding data: ${error}`))
-      ])
     })
     .catch(error => console.log(`Error seeding data: ${error}`));
 };
