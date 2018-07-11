5deaacf8bdcafa27f8ba02ccdf98dfc472302ec9
Jack Laird <jack.laird0@gmail.com>
2018-07-10 19:50:29 -0600

    Update seeding files to map over zone data

--- db/seeds/test/zones.js
+++ db/seeds/test/zones.js
@@ -1,34 +1,21 @@
+const { zoneData, plantData } = require('../seed-data'); 
+
 exports.seed = function (knex, Promise) {
+  const zones = zoneData.map(zone => {
+    return knex('zones').insert(zone, 'id')
+  });
   return knex('plants').del()
     .then(() => knex('zones').del())
     .then(() => {
       return Promise.all([
-        knex('zones').insert({ name: '1', lowTemp: -60, highTemp: -50 }, 'id'),
-        knex('zones').insert({ name: '2', lowTemp: -50, highTemp: -40}, 'id'),
-        knex('zones').insert({ name: '3', lowTemp: -40, highTemp: -30 }, 'id'),
-        knex('zones').insert({ name: '4', lowTemp: -30, highTemp: -20 }, 'id'),
-        knex('zones').insert({ name: '5', lowTemp: -20, highTemp: -10 }, 'id')
+        
+          ...zones
           .then(zones => {
-            return knex('plants').insert([
-              {
-                name: 'Aloe',
-                scientificName: 'sciAloe',
-                care: 'dont let it die',
-                moisture: 'not dry',
-                light: 'bright',
-                maintenance: 'often',
-                zone_id: zones[0]
-              },
-              {
-                name: 'Cactus',
-                scientificName: 'sciCactus',
-                care: 'dont put in closet',
-                moisture: 'dry',
-                light: 'DARK',
-                maintenance: 'never',
-                zone_id: zones[0]
-              }
-            ])
+            console.log(zones)
+            plantData.forEach(plant => {
+              plant.zone_id = zones[0]
+            })
+            return knex('plants').insert(plantData)
           })
           .then(() => console.log('Seeding complete!'))
           .catch(error => console.log(`Error seeding data: ${error}`))
