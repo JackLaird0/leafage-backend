d0adfa6b01826892c6c587117fcddaa73d7aa4b7
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-11 07:05:59 -0600

    Unnote scrapers, and add notes that explain which each one does.

--- scraper.js
+++ scraper.js
@@ -3,33 +3,38 @@ const nightmare = Nightmare({ show: true });
 const fs = require('fs')
 const plantURLs = require('./planturls');
 
-// let iterable = []
-// for (let i = 1; i < 177; i++) {
-//   iterable.push(i)
-// }
-// let allURLs = []
+//The following scraper goes to 176 pages of finegardening.com to gather all applicable URLs and writes them to './planturls.js'
 
-// iterable.reduce(function (accumulator, pageNumber) {
-//   return accumulator.then(function (results) {
-//     return nightmare.goto(`https://www.finegardening.com/plant-guide?paged=${pageNumber}`)
-//       .wait('.content-browser__results')
-//       .evaluate(() => {
-//         const cells = document.querySelectorAll('.content-browser__linked-image')
-//         var list = [].slice.call(cells);
-//         const pageResults = list.map(function (node, index) {
-//           return `${node}`
-//         });
-//         return pageResults
-//       })
-//       .then(function (result) {
-//         allURLs.push(...result)
-//       })
-//   });
-// }, Promise.resolve([])).then(function (results) {
-//   console.dir(allURLs);
-//   let setupFilePath = __dirname + '/./planturls.js'
-//   fs.writeFile(setupFilePath, JSON.stringify(allURLs))
-// });
+let iterable = []
+for (let i = 1; i < 177; i++) {
+  iterable.push(i)
+}
+let allURLs = []
+
+iterable.reduce(function (accumulator, pageNumber) {
+  return accumulator.then(function (results) {
+    return nightmare.goto(`https://www.finegardening.com/plant-guide?paged=${pageNumber}`)
+      .wait('.content-browser__results')
+      .evaluate(() => {
+        const cells = document.querySelectorAll('.content-browser__linked-image')
+        var list = [].slice.call(cells);
+        const pageResults = list.map(function (node, index) {
+          return `${node}`
+        });
+        return pageResults
+      })
+      .then(function (result) {
+        allURLs.push(...result)
+      })
+  });
+}, Promise.resolve([])).then(function (results) {
+  console.dir(allURLs);
+  let setupFilePath = __dirname + '/./planturls.js'
+  fs.writeFile(setupFilePath, JSON.stringify(allURLs))
+});
+
+// The following scraper takes all imported plantURLs and goes to each individual plant page
+// about 1750 pages of data are scraped.
 
 let plantObjects = []
 plantURLs.reduce(function (accumulator, url) {
