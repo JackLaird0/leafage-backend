1d074bdbcf54819499bf3808a87a7f9a65a13d6e
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-12 16:35:14 -0600

    Fix indention errors in seed data

--- db/seeds/seed-data.js
+++ db/seeds/seed-data.js
@@ -1,18 +1,18 @@
 const zoneData = [
-    { name: '1', lowTemp: -60, highTemp: -50 },
-    { name: '2', lowTemp: -50, highTemp: -40},
-    { name: '3', lowTemp: -40, highTemp: -30 },
-    { name: '4', lowTemp: -30, highTemp: -20 },
-    { name: '5', lowTemp: -20, highTemp: -10 },
-    { name: '6', lowTemp: -10, highTemp: 0 },
-    { name: '7', lowTemp: 0, highTemp: 10 },
-    { name: '8', lowTemp: 10, highTemp: 20 },
-    { name: '9', lowTemp: 20, highTemp: 30 },
-    { name: '10', lowTemp: 30, highTemp: 40 },
-    { name: '11', lowTemp: 40, highTemp: 50 },
-    { name: '12', lowTemp: 50, highTemp: 60 },
-    { name: '13', lowTemp: 60, highTemp: 70 }
-]
+  { name: '1', lowTemp: -60, highTemp: -50 },
+  { name: '2', lowTemp: -50, highTemp: -40},
+  { name: '3', lowTemp: -40, highTemp: -30 },
+  { name: '4', lowTemp: -30, highTemp: -20 },
+  { name: '5', lowTemp: -20, highTemp: -10 },
+  { name: '6', lowTemp: -10, highTemp: 0 },
+  { name: '7', lowTemp: 0, highTemp: 10 },
+  { name: '8', lowTemp: 10, highTemp: 20 },
+  { name: '9', lowTemp: 20, highTemp: 30 },
+  { name: '10', lowTemp: 30, highTemp: 40 },
+  { name: '11', lowTemp: 40, highTemp: 50 },
+  { name: '12', lowTemp: 50, highTemp: 60 },
+  { name: '13', lowTemp: 60, highTemp: 70 }
+];
 
 const plantData = [
   {
@@ -33,6 +33,6 @@ const plantData = [
     maintenance: 'never',
     zone_id: 2
   }
-]
+];
 
 module.exports = {zoneData, plantData };
